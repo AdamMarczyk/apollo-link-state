2b97b05497f2c41e77bb4b11f0f4d8578e313d3c
AdamMarczyk <adam.marczyk90@gmail.com>
2018-05-21 16:29:19 +0200

    implement display of query

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
-import React, { Component } from 'react';
+import React from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        App
-      </div>
-    );
+const GET_REPOSITORIES_OF_ORGANIZATION = gql`
+  {
+    organization(login: "the-road-to-learn-react") {
+      repositories(first: 20) {
+        edges {
+          node {
+            id
+            name
+            url
+            viewerHasStarred
+          }
+        }
+      }
+    }
   }
-}
+`;
+
+const App = () => (
+  <Query query={GET_REPOSITORIES_OF_ORGANIZATION}>
+    {({ data: { organization }, loading }) => {
+      if (loading || !organization) {
+        return <div>Loading ...</div>;
+      }
+
+      return (
+        <RepositoryList repositories={organization.repositories} />
+      );
+    }}
+  </Query>
+);
+
+const RepositoryList = ({ repositories }) => (
+  <ul>
+    {repositories.edges.map(({ node }) => {
+      return (
+        <li key={node.id}>
+          <a href={node.url}>{node.name}</a>
+        </li>
+      );
+    })}
+  </ul>
+);
 
 export default App;
